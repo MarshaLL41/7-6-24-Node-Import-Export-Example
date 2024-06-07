211dc0ec9987f8638cb0fe5e3063447d3688c0a2
Jignesh K <135426591+MarshaLL41@users.noreply.github.com>
2024-06-07 08:30:10 +0530

    Add files via upload

---
+++ export.js
@@ -0,0 +1,21 @@
+const FN1 = () => {
+    console.log("FUNCTION 1 EXPORT FILE");
+};
+
+const FN2 = () => {
+    console.log("FUNCTION 2 EXPORT FILE");
+};
+
+const FN3 = () => {
+    console.log("FUNCTION 3 EXPORT FILE");
+};
+
+const FN4 = () => {
+    console.log("FUNCTION 4 EXPORT FILE");
+};
+
+module.exports = { FN1, FN2, FN3, FN4 };
+
+// second method
+// module.exports = FN1;
+// second method
---
+++ import.js
@@ -0,0 +1,40 @@
+// FIRST METHOD ONE TO ONE FUNCTION
+// const FN_VARIABLE = require("./export");
+// FN_VARIABLE();
+// FIRST METHOD ONE TO ONE FUNCTION
+
+// SECOND METHOD MANY TO MANY FUNCTION
+// const { FN1 } = require("./export");
+// const { FN2 } = require("./export");
+// const { FN3 } = require("./export");
+// const { FN4 } = require("./export");
+// FN1();
+// FN2();
+// FN3();
+// FN4();
+// SECOND METHOD
+
+// THIRD METHOD
+// const FN_VARIABLE = require("./export");
+// FN_VARIABLE.FN1();
+// FN_VARIABLE.FN2();
+// FN_VARIABLE.FN3();
+// FN_VARIABLE.FN4();
+// THIRD METHOD
+
+// FOURTH METHOD
+// const FN_VARIABLE = require("./export");
+// const { FN1, FN2, FN3, FN4 } = FN_VARIABLE;
+// FN1();
+// FN2();
+// FN3();
+// FN4();
+// FOURTH METHOD
+
+// FIFTH METHOD
+// const { FN1, FN2, FN3, FN4 } = require("./export");
+// FN1();
+// FN2();
+// FN3();
+// FN4();
+// FIFTH METHOD
